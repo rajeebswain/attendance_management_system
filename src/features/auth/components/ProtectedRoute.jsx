6b15b7027a7623da5a5924eb2ff757b683495b1c
rajeebswain <rajeeb.mop@gmail.com>
2026-05-15 16:07:30 +0530

    testing for dashboard & ttendance

--- src/features/auth/components/ProtectedRoute.jsx
+++ src/features/auth/components/ProtectedRoute.jsx
@@ -1,59 +1,44 @@
+// // Redirect navigation
 // import { Navigate } from "react-router-dom";
 
+
+// // Authentication hook
 // import { useAuth } from "../context/AuthContext";
+
 
 // function ProtectedRoute({ children }) {
 
-//   const { user, loading } = useAuth();
+//   // Global auth state
+//   const {
 
+//     user,
+
+//     loading,
+
+//   } = useAuth();
+
+
+//   // Prevent flashing while loading
 //   if (loading) {
+
 //     return <div>Loading...</div>;
 //   }
 
+
+//   // Redirect if user not logged in
 //   if (!user) {
+
 //     return <Navigate to="/login" />;
 //   }
 
+
+//   // Allow protected page
 //   return children;
 // }
 
 // export default ProtectedRoute;
 
-// Redirect navigation
-import { Navigate } from "react-router-dom";
-
-
-// Authentication hook
-import { useAuth } from "../context/AuthContext";
-
-
 function ProtectedRoute({ children }) {
-
-  // Global auth state
-  const {
-
-    user,
-
-    loading,
-
-  } = useAuth();
-
-
-  // Prevent flashing while loading
-  if (loading) {
-
-    return <div>Loading...</div>;
-  }
-
-
-  // Redirect if user not logged in
-  if (!user) {
-
-    return <Navigate to="/login" />;
-  }
-
-
-  // Allow protected page
   return children;
 }
 
