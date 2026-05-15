a597bd90d0344725cc5ac8b072ea70eb3ea45220
rajeebswain <rajeeb.mop@gmail.com>
2026-05-15 18:37:20 +0530

    TEST

--- src/main.jsx
+++ src/main.jsx
@@ -1,66 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-
-// import { BrowserRouter } from 'react-router-dom';
-
-// // import './index.css';
-// import "./styles/globals.css";
-
-// import AppRoutes from './routes';
-
-// import { AuthProvider } from './features/auth/context/AuthContext';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//      <BrowserRouter>
-//         <AuthProvider>
-//           <AppRoutes/>
-//         </AuthProvider>
-//       </BrowserRouter>
-//   </React.StrictMode>
-// );
-
-// import React from "react";
-
-// import ReactDOM from "react-dom/client";
-
-
-// // Global styles
-// import "./styles/globals.css";
-
-
-// // Application routes
-// import AppRoutes from "./routes";
-
-
-// // Authentication provider
-// import {
-
-//   AuthProvider,
-
-// } from "./features/auth/context/AuthContext";
-
-
-// // Render React application
-// ReactDOM.createRoot(
-
-//   document.getElementById("root")
-
-// ).render(
-
-//   <React.StrictMode>
-
-//     {/* Global authentication wrapper */}
-//     <AuthProvider>
-
-//       <AppRoutes />
-
-//     </AuthProvider>
-
-//   </React.StrictMode>
-// );
-
-
 import React from "react";
 
 import ReactDOM from "react-dom/client";
@@ -89,7 +26,7 @@ ReactDOM.createRoot(
 
       <AuthProvider>
 
-        <AppRoutes />
+        <App />
 
       </AuthProvider>
 
