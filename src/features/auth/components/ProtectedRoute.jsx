833b4e4319cda7c2fd81103d8cbadebd3b6d40d1
rajeebswain <rajeeb.mop@gmail.com>
2026-05-15 16:15:42 +0530

    Tseting

--- src/features/auth/components/ProtectedRoute.jsx
+++ src/features/auth/components/ProtectedRoute.jsx
@@ -1,38 +1,44 @@
-// Redirect navigation
-import { Navigate } from "react-router-dom";
+// // Redirect navigation
+// import { Navigate } from "react-router-dom";
 
 
-// Authentication hook
-import { useAuth } from "../context/AuthContext";
+// // Authentication hook
+// import { useAuth } from "../context/AuthContext";
 
+
+// function ProtectedRoute({ children }) {
+
+//   // Global auth state
+//   const {
+
+//     user,
+
+//     loading,
+
+//   } = useAuth();
+
+
+//   // Prevent flashing while loading
+//   if (loading) {
+
+//     return <div>Loading...</div>;
+//   }
+
+
+//   // Redirect if user not logged in
+//   if (!user) {
+
+//     return <Navigate to="/login" />;
+//   }
+
+
+//   // Allow protected page
+//   return children;
+// }
+
+// export default ProtectedRoute;
 
 function ProtectedRoute({ children }) {
-
-  // Global auth state
-  const {
-
-    user,
-
-    loading,
-
-  } = useAuth();
-
-
-  // Prevent flashing while loading
-  if (loading) {
-
-    return <div>Loading...</div>;
-  }
-
-
-  // Redirect if user not logged in
-  if (!user) {
-
-    return <Navigate to="/login" />;
-  }
-
-
-  // Allow protected page
   return children;
 }
 
