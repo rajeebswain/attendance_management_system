6d98c6dbef2e3bd7f5332b0e79765c8dbd3f2abf
rajeebswain <rajeeb.mop@gmail.com>
2026-05-16 19:38:27 +0530

    Updated the Dashboard layout Path

--- src/features/attendance/pages/AdminAttendancePage.jsx
+++ src/features/attendance/pages/AdminAttendancePage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 import DashboardLayout
 
-from "../../../layouts/DashboardLayout";
+  from "../../../components/layout/DashboardLayout";
 
 import { getAllAttendance } from "../services/adminAttendanceService";
 
