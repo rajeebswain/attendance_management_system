894b9ba80a28b41cbd57d40363188801806b3e1d
rajeebswain <rajeeb.mop@gmail.com>
2026-05-16 15:32:46 +0530

    test 1

--- src/features/attendance/pages/EmployeeSelfAttendancePage.jsx
+++ src/features/attendance/pages/EmployeeSelfAttendancePage.jsx
@@ -1,9 +1,142 @@
+// import DashboardLayout
+
+// from "../../../components/layout/DashboardLayout";
+
+
+// function EmployeeSelfAttendancePage() {
+
+//   return (
+
+//     <DashboardLayout>
+
+//       <div className="space-y-6">
+
+//         <div
+//           className="
+//             bg-white
+//             p-6
+//             rounded-lg
+//           "
+//         >
+
+//           <h1
+//             className="
+//               text-2xl
+//               font-bold
+//             "
+//           >
+
+//             Employee Self Attendance
+
+//           </h1>
+
+//           <p className="mt-2">
+
+//             Basic page working.
+
+//           </p>
+
+//         </div>
+
+//       </div>
+
+//     </DashboardLayout>
+//   );
+// }
+
+// export default EmployeeSelfAttendancePage;
+
+
+import {
+
+  useEffect,
+
+  useState,
+
+} from "react";
+
+
 import DashboardLayout
 
 from "../../../components/layout/DashboardLayout";
 
 
+import {
+
+  useAuth,
+
+} from "../../auth/context/AuthContext";
+
+
+import {
+
+  getCurrentEmployee,
+
+} from "../services/selfAttendanceService";
+
+
 function EmployeeSelfAttendancePage() {
+
+  // Employee state
+  const [employee, setEmployee] = useState(null);
+
+
+  // Loading state
+  const [loading, setLoading] = useState(true);
+
+
+  // Current auth user
+  const { user } = useAuth();
+
+
+  // Load employee
+  useEffect(() => {
+
+    async function loadEmployee() {
+
+      try {
+
+        console.log("USER:", user);
+
+        const employeeData =
+
+          await getCurrentEmployee(
+
+            user.id
+          );
+
+        console.log(
+
+          "EMPLOYEE:",
+          employeeData
+        );
+
+        setEmployee(employeeData);
+
+      } catch (error) {
+
+        console.error(error);
+
+      } finally {
+
+        setLoading(false);
+      }
+    }
+
+    if (user) {
+
+      loadEmployee();
+    }
+
+  }, [user]);
+
+
+  // Loading UI
+  if (loading) {
+
+    return <div>Loading...</div>;
+  }
+
 
   return (
 
@@ -30,9 +163,23 @@ function EmployeeSelfAttendancePage() {
 
           </h1>
 
-          <p className="mt-2">
 
-            Basic page working.
+          <p className="mt-4">
+
+            Employee Name:
+            {" "}
+
+            {employee?.full_name}
+
+          </p>
+
+
+          <p>
+
+            Employee Code:
+            {" "}
+
+            {employee?.employee_code}
 
           </p>
 
