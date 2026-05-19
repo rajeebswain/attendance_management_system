2d7963d1e41447fcfb4171fb3fba21b8aedc86c9
rajeebswain <rajeeb.mop@gmail.com>
2026-05-19 19:04:34 +0530

    Adding Leave Apply Form

---
+++ src/features/leaves/components/LeaveForm.jsx
@@ -0,0 +1,119 @@
+import { useState } from "react";
+
+import Card from "../../../components/ui/Card";
+import Button from "../../../components/ui/Button";
+
+function LeaveForm() {
+
+const [leaveType,setLeaveType]=useState("");
+
+const [startDate,setStartDate]=useState("");
+
+const [endDate,setEndDate]=useState("");
+
+const [reason,setReason]=useState("");
+
+function handleSubmit(e){
+
+e.preventDefault();
+
+console.log({
+
+leaveType,
+startDate,
+endDate,
+reason
+
+});
+
+alert("Leave request submitted");
+
+}
+
+return(
+
+<Card>
+
+<h2 className="text-2xl font-bold mb-4">
+
+Apply Leave
+
+</h2>
+
+<form
+onSubmit={handleSubmit}
+className="space-y-4"
+>
+
+<select
+value={leaveType}
+onChange={(e)=>
+setLeaveType(e.target.value)
+}
+className="w-full border rounded p-3"
+>
+
+<option value="">
+Select Leave Type
+</option>
+
+<option value="casual">
+Casual Leave
+</option>
+
+<option value="sick">
+Sick Leave
+</option>
+
+<option value="earned">
+Earned Leave
+</option>
+
+<option value="emergency">
+Emergency Leave
+</option>
+
+</select>
+
+<input
+type="date"
+value={startDate}
+onChange={(e)=>
+setStartDate(e.target.value)
+}
+className="w-full border rounded p-3"
+/>
+
+<input
+type="date"
+value={endDate}
+onChange={(e)=>
+setEndDate(e.target.value)
+}
+className="w-full border rounded p-3"
+/>
+
+<textarea
+placeholder="Reason"
+value={reason}
+onChange={(e)=>
+setReason(e.target.value)
+}
+className="w-full border rounded p-3"
+/>
+
+<Button type="submit">
+
+Apply Leave
+
+</Button>
+
+</form>
+
+</Card>
+
+);
+
+}
+
+export default LeaveForm;
