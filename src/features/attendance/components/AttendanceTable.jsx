6892feb4fa506050127a61468674de876c2fcb2a
rajeebswain <rajeeb.mop@gmail.com>
2026-05-20 13:24:44 +0530

    Added the Overtime calculation

--- src/features/attendance/components/AttendanceTable.jsx
+++ src/features/attendance/components/AttendanceTable.jsx
@@ -1,91 +1,91 @@
 function AttendanceTable({
 
-    records,
-  
-  }) {
-  
-    return (
-  
-      <div className="overflow-x-auto">
-  
-        <table
-          className="
+  records,
+
+}) {
+
+  return (
+
+    <div className="overflow-x-auto">
+
+      <table
+        className="
             w-full
             bg-white
             rounded-lg
             overflow-hidden
           "
-        >
-  
-          {/* Table header */}
-          <thead className="bg-gray-200">
-  
-            <tr>
-  
-              <th className="p-4 text-left">
-                Employee
-              </th>
-  
-              <th className="p-4 text-left">
-                Designation
-              </th>
-  
-              <th className="p-4 text-left">
-                Date
-              </th>
-  
-              <th className="p-4 text-left">
-                Status
-              </th>
-  
-              <th className="p-4 text-left">
-                Check In
-              </th>
-  
-              <th className="p-4 text-left">
-                Check Out
-              </th>
-  
-            </tr>
-  
-          </thead>
-  
-  
-          {/* Table body */}
-          <tbody>
-  
-            {records.map((record) => (
-  
-              <tr
-                key={record.id}
-                className="border-b"
-              >
-  
-                <td className="p-4">
-  
-                  {record.employees?.full_name}
-  
-                </td>
-  
-                <td className="p-4">
-  
-                  {record.employees?.designation}
-  
-                </td>
-  
-                <td className="p-4">
-  
-                  {record.attendance_date}
-  
-                </td>
-  
-                <td className="p-4 capitalize">
-  
-                  {record.status}
-  
-                </td>
-  
-                {/* <td className="p-4">
+      >
+
+        {/* Table header */}
+        <thead className="bg-gray-200">
+
+          <tr>
+
+            <th className="p-4 text-left">
+              Employee
+            </th>
+
+            <th className="p-4 text-left">
+              Designation
+            </th>
+
+            <th className="p-4 text-left">
+              Date
+            </th>
+
+            <th className="p-4 text-left">
+              Status
+            </th>
+
+            <th className="p-4 text-left">
+              Check In
+            </th>
+
+            <th className="p-4 text-left">
+              Check Out
+            </th>
+
+          </tr>
+
+        </thead>
+
+
+        {/* Table body */}
+        <tbody>
+
+          {records.map((record) => (
+
+            <tr
+              key={record.id}
+              className="border-b"
+            >
+
+              <td className="p-4">
+
+                {record.employees?.full_name}
+
+              </td>
+
+              <td className="p-4">
+
+                {record.employees?.designation}
+
+              </td>
+
+              <td className="p-4">
+
+                {record.attendance_date}
+
+              </td>
+
+              <td className="p-4 capitalize">
+
+                {record.status}
+
+              </td>
+
+              {/* <td className="p-4">
   
                   {record.check_in}
   
@@ -97,37 +97,37 @@ function AttendanceTable({
   
                 </td> */}
 
-<td>
+              <td>
 
-{record.check_in_datetime
-? new Date(
-record.check_in_datetime
-).toLocaleTimeString()
-: "-"}
+                {record.check_in_datetime
+                  ? new Date(
+                    record.check_in_datetime
+                  ).toLocaleTimeString()
+                  : "-"}
 
-</td>
+              </td>
 
 
-<td>
+              <td>
 
-{record.check_out_datetime
-? new Date(
-record.check_out_datetime
-).toLocaleTimeString()
-: "-"}
+                {record.check_out_datetime
+                  ? new Date(
+                    record.check_out_datetime
+                  ).toLocaleTimeString()
+                  : "-"}
 
-</td>
-  
-              </tr>
-  
-            ))}
-  
-          </tbody>
-  
-        </table>
-  
-      </div>
-    );
-  }
-  
-  export default AttendanceTable;
+              </td>
+
+            </tr>
+
+          ))}
+
+        </tbody>
+
+      </table>
+
+    </div>
+  );
+}
+
+export default AttendanceTable;
