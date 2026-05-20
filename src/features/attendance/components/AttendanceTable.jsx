9b77033484988a29d1d1c312c1c590c398b26b2f
rajeebswain <rajeeb.mop@gmail.com>
2026-05-20 17:49:03 +0530

    Testing

--- src/features/attendance/components/AttendanceTable.jsx
+++ src/features/attendance/components/AttendanceTable.jsx
@@ -14,21 +14,21 @@ function AttendanceTable({
     <div className="overflow-x-auto">
 
 
-<input
-type="text"
-placeholder="Search employee..."
-value={search}
-onChange={(e)=>
-setSearch(e.target.value)
-}
-className="
+      <input
+        type="text"
+        placeholder="Search employee..."
+        value={search}
+        onChange={(e) =>
+          setSearch(e.target.value)
+        }
+        className="
 w-full
 border
 rounded
 p-3
 mb-4
 "
-/>
+      />
 
 
 
@@ -89,57 +89,57 @@ mb-4
         <tbody>
 
           {
-          
-          // records.map((record) => (
 
-          records
+            // records.map((record) => (
 
-.filter(
+            records
 
-(record)=>
+              .filter(
 
-record.employees?.full_name
-?.toLowerCase()
-.includes(
-search.toLowerCase()
-)
+                (record) =>
 
-)
+                  record.employees?.full_name
+                    ?.toLowerCase()
+                    .includes(
+                      search.toLowerCase()
+                    )
 
-.map((record)=>(
+              )
 
-            <tr
-              key={record.id}
-              className="border-b"
-            >
+              .map((record) => (
 
-              <td className="p-4">
+                <tr
+                  key={record.id}
+                  className="border-b"
+                >
 
-                {record.employees?.full_name}
+                  <td className="p-4">
 
-              </td>
+                    {record.employees?.full_name}
 
-              <td className="p-4">
+                  </td>
 
-                {record.employees?.designation}
+                  <td className="p-4">
 
-              </td>
+                    {record.employees?.designation}
 
-              <td className="p-4">
+                  </td>
 
-                {record.attendance_date}
+                  <td className="p-4">
 
-              </td>
+                    {record.attendance_date}
 
-              {/* <td className="p-4 capitalize">
+                  </td>
+
+                  {/* <td className="p-4 capitalize">
 
                 {record.status}
 
               </td> */}
 
-              <td className="p-4">
+                  <td className="p-4">
 
-<span className={`
+                    <span className={`
 
 px-2
 py-1
@@ -147,66 +147,65 @@ rounded
 text-white
 capitalize
 
-${
-record.status==="present"
-? "bg-green-500"
+${record.status === "present"
+                        ? "bg-green-500"
 
-:record.status==="late"
-? "bg-yellow-500"
+                        : record.status === "late"
+                          ? "bg-yellow-500"
 
-:record.status==="leave"
-? "bg-blue-500"
+                          : record.status === "leave"
+                            ? "bg-blue-500"
 
-:"bg-red-500"
-}
+                            : "bg-red-500"
+                      }
 
 `}>
 
-{record.status}
+                      {record.status}
 
-</span>
+                    </span>
 
-</td>
+                  </td>
 
-              <td>
+                  <td>
 
-                {record.check_in_datetime
-                  ? new Date(
-                    record.check_in_datetime
-                  ).toLocaleTimeString(
-                    'en-GB',
-                    {
-                      hour: '2-digit',
-                      minute: '2-digit',
-                      second: '2-digit',
-                      hour12: false
-                    }
-                  )
-                  : "-"}
+                    {record.check_in_datetime
+                      ? new Date(
+                        record.check_in_datetime
+                      ).toLocaleTimeString(
+                        'en-GB',
+                        {
+                          hour: '2-digit',
+                          minute: '2-digit',
+                          second: '2-digit',
+                          hour12: false
+                        }
+                      )
+                      : "-"}
 
-              </td>
+                  </td>
 
-              <td className="p-4">
+                  <td className="p-4">
 
-                {record.check_out_datetime
+                    {record.check_out_datetime
 
-                  ? new Date(
-                    record.check_out_datetime
-                  ).toLocaleTimeString(
-                    'en-GB',
-                    {
-                      hour: '2-digit',
-                      minute: '2-digit',
-                      second: '2-digit',
-                      hour12: false
-                    }
-                  )
+                      ? new Date(
+                        record.check_out_datetime
+                      ).toLocaleTimeString(
+                        'en-GB',
+                        {
+                          hour: '2-digit',
+                          minute: '2-digit',
+                          second: '2-digit',
+                          hour12: false
+                        }
+                      )
 
-                  : "-"}
+                      : "-"}
 
-              </td>
+                  </td>
 
-              {/* <td className="p-4">
+                  {/* <td className="p-4">
 
                 {new Date(
                   Number(record.worked_hours || 0)
@@ -218,22 +217,22 @@ record.status==="present"
               </td> */}
 
 
-              <td className="p-4">
+                  <td className="p-4">
 
-                {record.worked_hours
+                    {record.worked_hours
 
-                  ? new Date(
-                    Number(record.worked_hours)
-                    * 60 * 60 * 1000
-                  )
-                    .toISOString()
-                    .slice(11, 19)
+                      ? new Date(
+                        Number(record.worked_hours)
+                        * 60 * 60 * 1000
+                      )
+                        .toISOString()
+                        .slice(11, 19)
 
-                  : "00:00:00"}
+                      : "00:00:00"}
 
-              </td>
+                  </td>
 
-              {/* <td className="p-4">
+                  {/* <td className="p-4">
 
                 {new Date(
                   Number(record.overtime_hours || 0)
@@ -244,24 +243,24 @@ record.status==="present"
 
               </td> */}
 
-              <td className="p-4">
+                  <td className="p-4">
 
-                {record.overtime_hours
+                    {record.overtime_hours
 
-                  ? new Date(
-                    Number(record.overtime_hours)
-                    * 60 * 60 * 1000
-                  )
-                    .toISOString()
-                    .slice(11, 19)
+                      ? new Date(
+                        Number(record.overtime_hours)
+                        * 60 * 60 * 1000
+                      )
+                        .toISOString()
+                        .slice(11, 19)
 
-                  : "00:00:00"}
+                      : "00:00:00"}
 
-              </td>
+                  </td>
 
-            </tr>
+                </tr>
 
-          ))}
+              ))}
 
         </tbody>
 
