d656727358a446c9faf55ed26ed28f52b39851a9
rajeebswain <rajeeb.mop@gmail.com>
2026-05-22 18:03:49 +0530

    Error Rectification

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,12 +1,6 @@
 import { Link } from "react-router-dom";
 
 
-import {
-
-  isAdmin
-
-} from "../../utils/roleUtils";
-
 
 // Navigation constants
 import { SIDEBAR_LINKS } from "../../constants/navigation";
@@ -60,7 +54,7 @@ function Sidebar() {
         {/* Report Menu */}
 
 
-        {
+        {/* {
 
           isAdmin(
 
@@ -95,6 +89,73 @@ function Sidebar() {
 
     </aside>
   );
+}
+
+export default Sidebar; */}
+
+import { Link } from "react-router-dom";
+
+// Navigation constants
+import { SIDEBAR_LINKS } from "../../constants/navigation";
+
+function Sidebar() {
+
+return (
+
+<aside
+className="
+w-64
+min-h-screen
+bg-gray-900
+text-white
+"
+>
+
+<div className="p-6 border-b border-gray-700">
+
+<h1 className="text-2xl font-bold">
+
+AMS Panel
+
+</h1>
+
+</div>
+
+<nav className="p-4 space-y-2">
+
+{SIDEBAR_LINKS.map((item)=>(
+
+<Link
+key={item.path}
+to={item.path}
+className="
+block
+px-4
+py-3
+rounded
+hover:bg-gray-800
+transition
+"
+>
+
+{item.label}
+
+</Link>
+
+))}
+
+<Link to="/admin-attendance">
+
+Admin Attendance
+
+</Link>
+
+</nav>
+
+</aside>
+
+);
+
 }
 
 export default Sidebar;
