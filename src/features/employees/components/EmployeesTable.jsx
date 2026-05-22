d7274fe945689637754a0ff46e83052a403627e3
rajeebswain <rajeeb.mop@gmail.com>
2026-05-22 19:37:42 +0530

    Updated

--- src/features/employees/components/EmployeesTable.jsx
+++ src/features/employees/components/EmployeesTable.jsx
@@ -1,135 +1,3 @@
-// // Reusable button
-// import Button from "../../../components/ui/Button";
-
-
-// function EmployeesTable({
-
-//   employees,
-
-//   onDelete,
-
-// }) {
-
-//   return (
-
-//     <div className="overflow-x-auto">
-
-//       <table
-//         className="
-//           w-full
-//           bg-white
-//           rounded-lg
-//           overflow-hidden
-//         "
-//       >
-
-//         {/* Table header */}
-//         <thead className="bg-gray-200">
-
-//           <tr>
-
-//             <th className="p-4 text-left">
-//               Employee Code
-//             </th>
-
-//             <th className="p-4 text-left">
-//               Full Name
-//             </th>
-
-//             <th className="p-4 text-left">
-//               Email
-//             </th>
-
-//             <th className="p-4 text-left">
-//               Designation
-//             </th>
-
-//             <th className="p-4 text-left">
-//               Actions
-//             </th>
-
-//           </tr>
-
-//         </thead>
-
-
-//         {/* Table body */}
-//         <tbody>
-
-//           {employees.map((employee) => (
-
-//             <tr
-//               key={employee.id}
-//               className="border-b"
-//             >
-
-//               <td className="p-4">
-//                 {employee.employee_code}
-//               </td>
-
-//               <td className="p-4">
-//                 {employee.full_name}
-//               </td>
-
-//               <td className="p-4">
-//                 {employee.email}
-//               </td>
-
-//               <td className="p-4">
-//                 {employee.designation}
-//               </td>
-
-//               <td className="p-4">
-
-//                 {/* <Button
-//                   variant="danger"
-//                   onClick={() =>
-//                     onDelete(employee.id)
-//                   }
-//                 >
-//                   Delete
-//                 </Button> */}
-
-//                 <Button
-//                   onClick={() => {
-
-//                     const confirmDelete = window.confirm(
-//                       "Are you sure you want to delete this employee?"
-//                     );
-
-//                     if (confirmDelete) {
-
-//                       onDelete(employee.id);
-
-//                     }
-
-//                   }}
-//                 >
-
-//                   Delete
-
-//                 </Button>
-
-//               </td>
-
-//             </tr>
-
-//           ))}
-
-//         </tbody>
-
-//       </table>
-
-//     </div>
-//   );
-// }
-
-// export default EmployeesTable;
-
-
-
-
-
 // Reusable button
 import Button from "../../../components/ui/Button";
 
@@ -317,7 +185,7 @@ employee.is_active
 
 (
 
-<Button
+/* <Button
 onClick={()=>
 
 onDelete(
@@ -331,7 +199,43 @@ employee.id
 
 Deactivate
 
+</Button> */
+
+
+<Button
+
+onClick={()=>{
+
+const confirmDeactivate=
+
+window.confirm(
+
+"Do you want to deactivate this employee?"
+
+);
+
+if(
+
+confirmDeactivate
+
+){
+
+onDelete(
+
+employee.id
+
+);
+
+}
+
+}}
+
+>
+
+Deactivate
+
 </Button>
+
 
 )
 
@@ -339,7 +243,7 @@ Deactivate
 
 (
 
-<Button
+/* <Button
 onClick={()=>
 
 onRestore(
@@ -349,6 +253,42 @@ employee.id
 )
 
 }
+>
+
+Restore
+
+</Button> */
+
+
+
+<Button
+
+onClick={()=>{
+
+const confirmRestore=
+
+window.confirm(
+
+"Do you want to restore this employee?"
+
+);
+
+if(
+
+confirmRestore
+
+){
+
+onRestore(
+
+employee.id
+
+);
+
+}
+
+}}
+
 >
 
 Restore
